d5da971f3761ea3d7eb1a151c618cb7ebc34f620
Shah Foyez <shahfoyez7@gmail.com>
2022-12-12 17:44:54 +0600

    Update habi_jabi.php

--- habi_jabi.php
+++ habi_jabi.php
@@ -1,4 +1,7 @@
+// send action
 <button type="button" onClick="this.disabled=true; this.innerText='Sending…'; ">Send</button>
+
+// send action
 function submitAction() {
     $button = document.getElementById('foy_btn');
     $button.disabled = true;
