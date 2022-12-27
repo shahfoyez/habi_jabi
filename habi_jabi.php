d81d945007acd6dc72bcc7f6c4639b7f48e72cff
Shah Foyez <shahfoyez7@gmail.com>
2022-12-27 19:41:54 +0600

    Update habi_jabi.php

--- habi_jabi.php
+++ habi_jabi.php
@@ -26,3 +26,40 @@ $trips = Trip::selectRaw("DATE_FORMAT(`start`, '%M-%y') as monthYear, year(`star
         ->orderBy('year', "DESC")
         ->orderBy('month', "DESC")
         ->get()->take(12);
+
+
+<script src="https://maps.googleapis.com/maps/api/js?key="></script>
+<script>
+   var map;
+    var markers = [];
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 24.8949, lng: 91.8687},
+            zoom: 8
+        });
+        function addMarker(lat, lng) {
+            var marker = new google.maps.Marker({
+                position: {lat: lat, lng: lng},
+                map: map
+            });
+            markers.push(marker);
+        }
+        function getLocations() {
+            $.ajax({
+                url: '/route/hello',
+                type: 'GET',
+                success: function(data) {
+                    console.log(data);
+                    for (var i = 0; i < data.length; i++) {
+                        addMarker(data[i].latitude, data[i].longitude);
+                    }
+                }
+            });
+        }
+        setInterval(function() {
+        getLocations();
+        }, 5000);
+
+    }
+</script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=&callback=initMap"></script>
