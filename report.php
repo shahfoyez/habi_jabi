415f884fb434d9053dbf5b280828392ff8200422
Shah Foyez <shahfoyez7@gmail.com>
2023-05-02 12:52:27 -0700

    Create report.php

---
+++ report.php
@@ -0,0 +1,25 @@
+                      +---------------------------+
+                      |      Vehicle Tracking     |
+                      |      and Management       |
+                      +---------------------------+
+                                     |
+                                     |
+                                     |
+                   +-----------------+-----------------+
+                   |                                     |
+     +-------------+------+                      +-------+------+
+     |   Student         |                      |    Admin        |
+     +--------------------+                   +----------------+
+     |  - View vehicle location               |  - Manage vehicles
+     |  - Send feedback                        |  - Generate trip reports
+     |  - Receive notices                      |  - Respond to queries
+     |                                        |  - Generate KPL reports
+     +--------------------+                   +----------------+
+                                    |
+                                    |
+                                    |
+                          +----------+-----------+
+                          |    GPS Unit           |
+                          +-----------------------+
+                          |  - Send location data  |
+                          +-----------------------+
