631e7544c561f873bcfdc4c650492932c61d9c2b
Shah Foyez <shahfoyez7@gmail.com>
2023-07-20 11:01:04 +0600

    Create file_zila_path_association.php

---
+++ file_zila_path_association.php
@@ -0,0 +1,9 @@
+// for php storm
+css "C:\Users\Staff Asia - PC\AppData\Local\JetBrains\Toolbox\apps\PhpStorm\ch-0\231.9225.18\bin\phpstorm64.exe" %f
+js "C:\Users\Staff Asia - PC\AppData\Local\JetBrains\Toolbox\apps\PhpStorm\ch-0\231.9225.18\bin\phpstorm64.exe" %f
+php "C:\Users\Staff Asia - PC\AppData\Local\JetBrains\Toolbox\apps\PhpStorm\ch-0\231.9225.18\bin\phpstorm64.exe" %f
+
+// for vs code
+css "C:\Users\Staff Asia - PC\AppData\Local\Programs\Microsoft VS Code\Code.exe" %f
+js "C:\Users\Staff Asia - PC\AppData\Local\Programs\Microsoft VS Code\Code.exe" %f
+php "C:\Users\Staff Asia - PC\AppData\Local\Programs\Microsoft VS Code\Code.exe" %f
