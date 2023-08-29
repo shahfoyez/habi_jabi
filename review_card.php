ff01d6c2691f195a47ede1487282ebd80f8dbd27
Shah Foyez <shahfoyez7@gmail.com>
2023-08-29 11:25:38 +0600

    Create review_card.php

---
+++ review_card.php
@@ -0,0 +1,44 @@
+<style>
+  .foy-flex {
+      display: flex;
+      align-items: center;
+  }
+  .foy-avatar{
+      width: 63px;
+      height: 63px;
+      border-radius: 50px !important;
+      margin-right: 10px;
+  }
+  .foy-author h4{
+      margin: 0;
+      color: #000;
+      font-family: 'Inter', serif;
+      font-size: 15.789px;
+      font-weight: 700;
+  }
+  .foy-author p{
+      color: #000;
+      font-family: 'Inter', serif;
+      font-size: 10.526px;
+      font-weight: 400;
+      margin: 5px 0px;
+  }
+  .foy-review-content{
+      margin-top: 20px;
+      color: #000;
+      font-family: Inter;
+      font-size: 10.526px;
+      font-weight: 400;
+  }
+</style>
+<div class="foy-review-main foy-review-1">
+    <div class="foy-flex">
+        <img class="foy-avatar" src="https://www.libm.co.uk/wp-content/uploads/2022/03/ezgif.com-gif-maker-41.webp">
+        <div class="foy-author">
+            <h4>Tanvir Ahmed Ashiq</h4>
+            <p>Management Diploma</p>
+            <img src="http://www.libm.co.uk/wp-content/uploads/2023/08/Frame-1000003877-1.png">
+        </div>
+    </div>
+    <p class="foy-review-content">I really love LIBM and it is one of the great institutes I have ever seen. I have a great experience to do all the course I still love. Thank you very much for your great help. I really appreciate your excellent service. Thank you once again for such a kinda course LIBM provides.</p>
+</div>
