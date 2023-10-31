f7522203a9a4f76e5fddec505a2f6d08f690dab1
Shah Foyez <shahfoyez7@gmail.com>
2023-11-01 01:01:39 +0600

    Update htaccess.php

--- htaccess.php
+++ htaccess.php
@@ -18,3 +18,38 @@ RewriteRule ^(.*)$ "https\:\/\/anwars\-restaurants\.com\/$1" [R=301,L]
   AddHandler application/x-httpd-ea-php74___lsphp .php .php7 .phtml
 </IfModule>
 # php -- END cPanel-generated handler, do not edit
+
+
+/////////////////////////////////////////////////////////////////
+
+
+<IfModule mod_rewrite.c>
+    RewriteEngine On
+
+    # Remove the public/ directory from URL
+    RewriteRule ^(.*)$ public/$1 [L]
+
+    # Handle the public/ directory
+    RewriteCond %{REQUEST_URI} !^public/
+    RewriteRule ^(.*)$ public/index.php [L]
+</IfModule>
+
+# Protect your .env file
+<Files .env>
+    Order allow,deny
+    Deny from all
+</Files>
+
+# Protect against unauthorized access to sensitive files
+<FilesMatch "\.(env|json|config|lock|gitignore|htaccess)$">
+    Order allow,deny
+    Deny from all
+</FilesMatch>
+
+# Redirect to HTTPS (if not using a load balancer)
+<IfModule mod_rewrite.c>
+    RewriteEngine On
+    RewriteCond %{HTTPS} off
+    RewriteRule ^ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]
+</IfModule>
+
